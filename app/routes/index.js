2cdfbb804ce2e74909f3d7b49b69a16ff2daee3a
Wolfgang C. Strack <wolfgangcstrack@gmail.com>
2016-06-17 17:18:57 -0700

    Set up valid date routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,21 @@
 var path = process.cwd();
 
+// this regex will match natural language date input in the url
+var nat_lang_match = /^\/(Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sept?|September|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)(%20)*?[0-3]?[0-9],?(%20)*?[0-9]+$/;
+var unix_timestamp = /^\/[+-]?[0-9]+$/;
+
 module.exports = function (app) {
-	app.route('/')
-		.get(function (req, res) {
-			res.sendFile(path + '/public/index.html');
-		});
+	app.get('/', function (req, res) {
+		res.sendFile(path + '/public/index.html');
+	});
+
+	app.get(nat_lang_match, function(req, res) {
+		// TODO: call date-converting controller
+		res.json({dummy: 'text'});
+	});
+
+	app.get(unix_timestamp, function(req, res) {
+		// TODO: call date-converting controller
+		res.json({foo: 'bar'});
+	});
 };
