adad333f824a615a4582bef74a1558972d6a9c91
Wolfgang C. Strack <wolfgangcstrack@gmail.com>
2016-06-22 00:00:51 -0700

    Fix sample dates. Prototype timestampFromString function

--- app/controllers/dateConverter.js
+++ app/controllers/dateConverter.js
@@ -1,7 +1,8 @@
 function DateConverter () {
   this.timestampFromString = function (req, res) {
-    // TODO
-    res.json({sent: req.url.slice(1)});
+    var natural = req.url.slice(1).split('%20').join(' ');
+    var unix    = new Date(natural).getTime();
+    res.json({'unix': unix, 'natural': natural});
   };
   this.timestampFromUnix = function (req, res) {
     // TODO
