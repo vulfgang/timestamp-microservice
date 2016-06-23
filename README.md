2113ea870d796d0a9a853910b83a8be67ed05282
Wolfgang C. Strack <wolfgangcstrack@gmail.com>
2016-06-23 16:08:28 -0700

    Prototype timestampFromUnix function

--- README.md
+++ README.md
@@ -5,8 +5,8 @@
 ### Example usage:
 
 `https://test.url.doge/December%2015,%202015`  
-`https://test.url.doge/1450137600`
+`https://test.url.doge/1450166400000`
 
 ### Example output:
 
-`{ "unix": 1450137600, "natural": "December 15, 2015" }`
+`{ "unix": 1450166400000, "natural": "December 15, 2015" }`
--- app/controllers/dateConverter.js
+++ app/controllers/dateConverter.js
@@ -1,12 +1,19 @@
 function DateConverter () {
+  var months = ["January", "February", "March", "April",
+                "May", "June", "July", "August", "September",
+                "October", "November", "December"];
+
   this.timestampFromString = function (req, res) {
     var natural = req.url.slice(1).split('%20').join(' ');
     var unix    = new Date(natural).getTime();
     res.json({'unix': unix, 'natural': natural});
   };
+
   this.timestampFromUnix = function (req, res) {
-    // TODO
-    res.json({sent: req.url.slice(1)});
+    var date = new Date(parseInt(req.url.slice(1)));
+    var natural = months[date.getMonth()]+" "+date.getDate()+", "+date.getFullYear();
+    var unix = date.getTime();
+    res.json({'unix': unix, 'natural': natural});
   };
 }
 
