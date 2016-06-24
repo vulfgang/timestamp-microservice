3279a8057c53252e6757960be0afcc62af78ac8c
Wolfgang C. Strack <wolfgangcstrack@gmail.com>
2016-06-23 22:16:13 -0700

    Clean up

--- app/controllers/dateConverter.js
+++ app/controllers/dateConverter.js
@@ -1,14 +1,19 @@
 function DateConverter () {
-  var months = ["January", "February", "March", "April",
-                "May", "June", "July", "August", "September",
-                "October", "November", "December"];
+  // used for creating natural-language date from unix timestamp
+  var months = [
+      "January", "February", "March", "April",
+      "May", "June", "July", "August",
+      "September", "October", "November", "December"
+  ];
 
+  // gets timestamp for date in format "Month DD, YYYY"
   this.timestampFromString = function (req, res) {
     var natural = req.url.slice(1).split('%20').join(' ');
     var unix    = new Date(natural).getTime();
     res.json({'unix': unix, 'natural': natural});
   };
 
+  // gets timestamp for date in format of unix timestamp
   this.timestampFromUnix = function (req, res) {
     var date = new Date(parseInt(req.url.slice(1)));
     var natural = months[date.getMonth()]+" "+date.getDate()+", "+date.getFullYear();
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,15 +1,10 @@
 var path = process.cwd();
+var DateConverter = require(path + '/app/controllers/dateConverter.js');
 
 // this regex will match natural language date input in the url
 var nat_lang_match = /^\/(Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sept?|September|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)(%20)*?[0-3]?[0-9],?(%20)*?[0-9]+$/;
-/* Same natural language matching regex but neater for reading I guess:
-	/^\/(Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|
-	Jul(?:y)?|Aug(?:ust)?|Sept?|September|Oct(?:ober)?|Nov(?:ember)?|
-	Dec(?:ember)?)(%20)*?[0-3]?[0-9],?(%20)*?[0-9]+$/;
-*/
+// this regex will match any unix timestamp in the url, even if negative
 var unix_timestamp = /^\/[+-]?[0-9]+$/;
-
-var DateConverter = require(path + '/app/controllers/dateConverter.js');
 
 module.exports = function (app) {
 	app.get('/', function (req, res) {
@@ -18,15 +13,18 @@ module.exports = function (app) {
 
 	var dateConverter = new DateConverter();
 
+	// matches date strings in format "/Month DD, YYYY"
 	app.get(nat_lang_match, function(req, res) {
 		dateConverter.timestampFromString(req, res);
 	});
 
+	// matches unix timestamps in format "/#####..."
 	app.get(unix_timestamp, function(req, res) {
 		dateConverter.timestampFromUnix(req, res);
 	});
 
+	// should catch any other routes not covered
 	app.get('/:anything_else', function(req, res) {
 		res.json({unix: null, natural: null});
-	})
+	});
 };
